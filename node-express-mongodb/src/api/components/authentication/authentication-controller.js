fc23a3a210275a3f5b810d6849f232b236cf5fd2
IkanDongo <noel.535230067@stu.untar.ac.id>
2024-05-01 16:51:46 +0700

    sudah melakukan sedikit perubahan terhadap error karena tidak masuk db

--- node-express-mongodb/src/api/components/authentication/authentication-controller.js
+++ node-express-mongodb/src/api/components/authentication/authentication-controller.js
@@ -1,5 +1,8 @@
+const { attempt } = require('joi');
 const { errorResponder, errorTypes } = require('../../../core/errors');
+const { email } = require('../../../models/users-schema');
 const authenticationServices = require('./authentication-service');
+const { Attempt } = require('../../../models');
 
 /**
  * Handle login request
@@ -50,16 +53,75 @@ async function login(request, response, next) {
  */
 async function createAttempt(request, response, next) {
   try {
-    const name = request.body.name;
+    const email = request.body.email;
     const attempt = request.body.attempt;
 
-  return response.status(200).json({ name, attempt });
+    const success = authenticationServices.createAttempt(email, attempt);
+    if (!success){
+    throw errorResponder(
+      errorTypes.UNPROCESSABLE_ENTITY,
+      'Failed to create Attempt'
+    );
+  }
+
+  return response.status(200).json({ email, attempt });
 } catch (error) {
   return next(error);
 }
+}
+/**
+ * Handle create user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function updateAttempt(request, response, next) {
+  try {
+    const email = request.body.email;
+    const attempt = request.body.attempt;
+    
+const success = await authenticationServices.updateUser(email, attempt);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to update user'
+      );
+    }
+    return response.status(200).json({email,attempt});
+  } catch (error) {
+    return next(error);
+  }
+}
+
+/**
+ * Handle delete user request
+ * @param {object} request - Express request object
+ * @param {object} response - Express response object
+ * @param {object} next - Express route middlewares
+ * @returns {object} Response object or pass an error to the next route
+ */
+async function getLoginAttempts(request, response, next) {
+  try {
+    const attempt = request.params.attempt;
+
+    const success = await authenticationServices.getLoginAttempts(attempt);
+    if (!success) {
+      throw errorResponder(
+        errorTypes.UNPROCESSABLE_ENTITY,
+        'Failed to delete user'
+      );
+    }
+
+    return response.status(200).json({attempt});
+  } catch (error) {
+    return next(error);
+  }
 }
 
 module.exports = {
   login,
   createAttempt,
+  updateAttempt,
+  getLoginAttempts
 };
